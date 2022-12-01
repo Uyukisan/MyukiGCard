a4e795ce70ada4e67e858c902a70e8a2a77ffcee
Uyukisan <hzbh1314xf@163.com>
2022-12-01 18:46:52 +0800

    add hitokoto

--- README.md
+++ README.md
@@ -26,13 +26,14 @@ window.MyNavCard = $MGC({
 	*/
 	info: "欢迎使用MyukiGCard",
 	/*
-	info: 个性签名、网站口号、信息等
+	info: 个性签名、网站口号、信息等，为空或者不设置将不显示
+	默认值: 空
 	*/
 	z_index: 9999,
 	/*
 	不用解释，默认值为9999
 	*/
-	blur: ".container",
+	blur: "#write",
 	/*
 	blur: 想要模糊化处理的页面元素，取值可以是.className（类名）、#idName（id属性值）、tagName（标签名）
 	*/
@@ -58,13 +59,50 @@ window.MyNavCard = $MGC({
 	2: 根据时间，下午6时～上午6时打开夜间模式
 	3: 常开夜间模式
 	*/
+	maxWidth: "480px",
+	/*
+	maxWidth: MyukiGCard的最大宽度
+	默认值: 480px
+	*/
+	fontFamily: "",
+	/*
+	fontFamily: 字体，为空或不设置将使用默认字体
+	*/
+	hitokoto: {
+		"enable": true,
+		/*
+		enable: 是否启用一言API
+		默认值: false
+		*/
+		"cats": ["d", "i", "k"],
+		/*
+		cats: 句子类型，详见：https://developer.hitokoto.cn/sentence/#句子类型-参数
+		默认值: []
+		a: 动画, b: 漫画, c: 游戏, d: 文学, e: 原创, f: 网络, 
+		g: 其他, h: 影视, i: 诗词, j: 网易, k: 哲学, i: 抖机灵
+		*/
+		"offline": "兰博基尼和法拉利能一样吗？分手！",
+		/*
+		offline: 一言API请求错误时用于替换的离线语句
+		*/
+		"color": "#70a1ff",
+		/*
+		color: 一言语句颜色
+		默认值: #70a1ff
+		*/
+	},
+	/*
+	hitokoto: 一言API
+	注: 不兼容IE
+	*/
 	links: [{
 		"title": "My Blog",
 		"url": "https://stackblog.cf/",
-		"type": "primary"
+		"type": "primary",
+		"target": "_blank"
 	}, {
 		"title": "My GitHub",
-		"url": "http://github.com/Uyukisan"
+		"url": "https://github.com/Uyukisan",
 	}, {
 		"title": "MyukiGCard Usage",
 		"url": "https://github.com/Uyukisan/MyukiGCard",
@@ -73,12 +111,16 @@ window.MyNavCard = $MGC({
 	导航链接列表
 	title: 链接的标题
 	url: 链接地址
+	
 	type: 链接按钮的类型，包括：
 	default | primary | secondary | success | danger | waring | info | light | dark | link
 	如果不提供type，默认为dafault
+	
+	target: 在何处打开链接，包括：
+	_blank（新窗口）| _self(默认) | _parent(父框架) | _top(整个窗口) | framename(指定的框架)
+	如果不提供target，将使用默认
 	*/
 });
-
 
 /*
  方法：close()
--- js/MyukiGCard.js
+++ js/MyukiGCard.js
@@ -10,28 +10,42 @@
 	var defaultSetting = {
 		icon: "https://stackblog.cf/img/avatar.jpg",
 		name: "Stack Dev",
-		info: "欢迎使用Myuki Guidance Card",
+		info: "",
 		z_index: 9999,
 		blur: ".container",
 		lang: "zh-CN",
 		mini: true,
 		darkmode: 1,
+		maxWidth: "480px",
+		fontFamily: "",
+		hitokoto: {
+			"enable": false,
+			"cats": [],
+			"color": "#70a1ff",
+			"offline": "我是一条格言"
+		},
 		links: [{
 			"title": "My Blog",
 			"url": "https://stackblog.cf/",
 			"type": "primary"
 		}, {
 			"title": "My GitHub",
-			"url": "http://github.com/Uyukisan"
+			"url": "https://github.com/Uyukisan"
 		}, {
 			"title": "My jq22",
 			"url": "https://www.jq22.com/mem1320295"
 		}]
 	};
 	var i18n = new Map([
-		["zh-CN",{"close":"关闭窗口"}],
-		["zh-TW",{"close":"關閉窗口"}],
-		["en-US",{"close":"Ignore it"}]
+		["zh-CN", {
+			"close": "关闭窗口"
+		}],
+		["zh-TW", {
+			"close": "關閉窗口"
+		}],
+		["en-US", {
+			"close": "Ignore it"
+		}]
 	]);
 	var darkmode = [0, 1, 2, 3];
 	Object.freeze(defaultSetting);
@@ -42,38 +56,76 @@
 		constructor: MyukiGCard,
 
 		init: function(option, undefined) {
-			this.help();
-			this._setting = extend({}, defaultSetting, option);
+			let _this = this;
+			_this.help();
+			_this._setting = extend({}, defaultSetting, option);
 			let myukiGCard = document.createElement("div");
 			addClass("myuki-gcard_mask", myukiGCard);
-			myukiGCard.style.zIndex = parseInt(this._setting.z_index);
+			myukiGCard.style.zIndex = parseInt(_this._setting.z_index);
 			let cardBox = document.createElement("div");
 			addClass("myuki-gcard_box", cardBox);
+			_this._setting.fontFamily != "" ? cardBox.style.fontFamily = _this._setting.fontFamily : "";
+			cardBox.style.maxWidth = _this._setting.maxWidth;
 			let cardIcon = document.createElement("div");
 			addClass("myuki-gcard_icon", cardIcon);
 			let img = document.createElement("img");
-			img.setAttribute("src", this._setting.icon);
-			img.setAttribute("alt", this._setting.name);
+			img.setAttribute("src", _this._setting.icon);
+			img.setAttribute("alt", _this._setting.name);
 			cardIcon.appendChild(img);
 			cardBox.appendChild(cardIcon);
 			let cardName = document.createElement("div");
 			addClass("myuki-gcard_title", cardName);
-			cardName.innerText = this._setting.name;
+			cardName.innerText = _this._setting.name;
 			cardBox.appendChild(cardName);
 			let cardInfo = document.createElement("div");
 			addClass("myuki-gcard_info", cardInfo);
-			let p = document.createElement("p");
-			p.innerText = this._setting.info;
-			cardInfo.appendChild(p);
+			if (_this._setting.info != "") {
+				let p = document.createElement("p");
+				p.innerText = _this._setting.info;
+				cardInfo.appendChild(p);
+			}
+			// 添加一言API，不兼容IE
+			if (_this._setting.hitokoto.enable == true) {
+				let hitokoto = {};
+				let catStr = _this._setting.hitokoto.cats ? _this._setting.hitokoto.cats.map(c => "c=" + c)
+					.join("&") : defaultSetting.hitokoto.cats.map(c => "c=" + c).join("&");
+				fetch('https://v1.hitokoto.cn/?' + catStr)
+					.then((res) => res.json()).then((data) => {
+						hitokoto.text = data.hitokoto;
+						hitokoto.uuid = data.uuid;
+					})
+					.catch((err) => {
+						console.error("一言API请求错误：" + err);
+						hitokoto.text = _this._setting.hitokoto.offline ? _this._setting.hitokoto
+							.offline :
+							defaultSetting.hitokoto.offline;
+					})
+					.finally(() => {
+						hitokoto.text = `「${hitokoto.text}」`;
+						let p = document.createElement("p");
+						let a = document.createElement("a");
+						a.setAttribute("target", "_blank");
+						a.innerText = hitokoto.text;
+						a.setAttribute("href", `https://hitokoto.cn?uuid=${hitokoto.uuid}`);
+						p.appendChild(a);
+						p.setAttribute("class", "myuki-gcard_hitokoto");
+						p.style.color = _this._setting.hitokoto.color ? _this._setting.hitokoto.color :
+							defaultSetting.hitokoto.color;
+						cardInfo.appendChild(p);
+					});
+
+			}
 			cardBox.appendChild(cardInfo);
 			let cardBtnList = document.createElement("div");
 			addClass("myuki-gcard_btn-list", cardBtnList);
-			let links = this._setting.links;
+			let links = _this._setting.links;
 			for (let i = 0; i < links.length; i++) {
 				let btn = document.createElement("a");
 				addClass("myuki-gcard_btn", btn);
-				links[i].type ? addClass("btn-"+links[i].type, btn) : addClass("btn-default", btn);
-				links[i].type ? btn.setAttribute("btn-type", links[i].type) : btn.setAttribute("btn-type", "btn-default");
+				links[i].type ? addClass("btn-" + links[i].type, btn) : addClass("btn-default", btn);
+				links[i].type ? btn.setAttribute("btn-type", links[i].type) : btn.setAttribute("btn-type",
+					"btn-default");
+				links[i].target ? btn.setAttribute("target", links[i].target) : "";
 				btn.setAttribute("href", links[i].url);
 				btn.innerText = links[i].title;
 				cardBtnList.appendChild(btn);
@@ -84,54 +136,59 @@
 			closeBtn.setAttribute("href", "javascript: void (0);");
 			closeBtn.setAttribute("id", "myuki-gcard_close");
 			closeBtn.setAttribute("btn-type", "btn-close");
-			let lang = i18n.get(this._setting.lang) ? this._setting.lang : document.documentElement.lang != "" ? document.documentElement.lang : defaultSetting.lang;
-			closeBtn.innerText = i18n.get(lang) ? i18n.get(lang).close : i18n.get(defaultSetting.lang).close;
-			let _this = this;
-			closeBtn.addEventListener("click",function(){
+			let lang = i18n.get(_this._setting.lang) ? _this._setting.lang : document.documentElement
+				.lang !=
+				"" ? document.documentElement.lang : defaultSetting.lang;
+			closeBtn.innerText = i18n.get(lang) ? i18n.get(lang).close : i18n.get(defaultSetting.lang)
+				.close;
+			closeBtn.addEventListener("click", function() {
 				_this.close();
 			});
 			cardBtnList.appendChild(closeBtn);
 			cardBox.appendChild(cardBtnList);
 			// darkmode
-			let dmode = darkmode.includes(this._setting.darkmode) ? this._setting.darkmode : defaultSetting.darkmode;
-			switch(dmode){
+			let dmode = darkmode.includes(_this._setting.darkmode) ? _this._setting.darkmode :
+				defaultSetting
+				.darkmode;
+			switch (dmode) {
 				case 0:
 					removeClass("darkmode", cardBox);
 					break;
 				case 1:
-					window.matchMedia("(prefers-color-scheme: dark)").matches ? addClass("darkmode", cardBox) : "";
+					window.matchMedia("(prefers-color-scheme: dark)").matches ? addClass("darkmode",
+						cardBox) : "";
 					break;
 				case 2:
 					let hour = new Date().getHours();
-					hour <6 || hour>=18 ? addClass("darkmode", cardBox) : "";
+					hour < 6 || hour >= 18 ? addClass("darkmode", cardBox) : "";
 					break;
 				case 3:
 					addClass("darkmode", cardBox);
 				default:
 					break;
-					
+
 			}
 			myukiGCard.appendChild(cardBox);
 			document.body.appendChild(myukiGCard);
-			if(this._setting.mini == true){
+			if (_this._setting.mini == true) {
 				let cardMini = document.createElement("div");
 				addClass("myuki-gcard_mini", cardMini);
 				let miniImg = document.createElement("img");
-				miniImg.setAttribute("src", this._setting.icon);
+				miniImg.setAttribute("src", _this._setting.icon);
 				cardMini.appendChild(miniImg);
-				cardMini.style.zIndex = this._setting.z_index;
-				cardMini.addEventListener("click",function(){
-					_this.open(()=>{
+				cardMini.style.zIndex = _this._setting.z_index;
+				cardMini.addEventListener("click", function() {
+					_this.open(() => {
 						removeClass("shown", cardMini);
 					});
 				});
 				document.body.appendChild(cardMini);
 			}
-			let container = document.querySelectorAll(this._setting.blur)[0];
-			if(container != undefined){
+			let container = document.querySelectorAll(_this._setting.blur)[0];
+			if (container != undefined) {
 				addClass("blur", container);
 			}
-			
+
 			this._GCard = myukiGCard;
 			return this;
 		},
@@ -141,40 +198,41 @@
 		},
 
 		close: function(closecallback) {
-			if(!this._closed){
+			if (!this._closed) {
 				this._closed = true;
-			}else{
-				console.log("MyukiGCard has been closed.");
+			} else {
+				console.warn("MyukiGCard closed.");
 				return;
 			}
 			let GCard = this._GCard;
 			let cardBox = GCard.querySelectorAll(".myuki-gcard_box")[0];
-			
+
 			cardBox.addEventListener("animationend", cfun);
 			// cardBox.removeEventListener("animationend", cfun);
 
 			addClass("closed", cardBox);
-			if(typeof closecallback == "function"){
+			if (typeof closecallback == "function") {
 				closecallback();
 			}
 			let _this = this;
-			function cfun(){
+
+			function cfun() {
 				addClass("hidden", GCard);
 				let container = document.querySelectorAll(_this._setting.blur)[0];
 				let cardMini = document.querySelectorAll(".myuki-gcard_mini")[0];
-				if(container != undefined){
+				if (container != undefined) {
 					removeClass("blur", container);
 				}
 				cardMini != undefined ? addClass("shown", cardMini) : "";
 				cardBox.removeEventListener("animationend", cfun);
 			}
 		},
-		
+
 		open: function(opencallback) {
-			if(this._closed){
+			if (this._closed) {
 				this._closed = false;
-			}else{
-				console.log("MyukiGCard has been opened.");
+			} else {
+				console.warn("MyukiGCard opened.");
 				return;
 			}
 			let GCard = this._GCard;
@@ -183,20 +241,29 @@
 
 			removeClass("closed", cardBox);
 			let container = document.querySelectorAll(this._setting.blur)[0];
-			if(container != undefined){
+			if (container != undefined) {
 				addClass("blur", container);
 			}
-			if(typeof opencallback == "function"){
+			if (typeof opencallback == "function") {
 				opencallback();
 			}
 		},
-		
+
 		help: function() {
-			console.log("⛄️欢迎使用MyukiGCard:",{"autor": "Stack Dev","github":"https://github.com/Uyukisan", "usage": "https://github.com/Uyukisan/MyukiGCard", "preview": "https://uyukisan.github.io/MyukiGCard/"});
-			return {"autor": "Stack Dev","github":"https://github.com/Uyukisan", "usage": "https://github.com/Uyukisan/MyukiGCard", "preview": "https://uyukisan.github.io/MyukiGCard/"};
+			console.info("⛄️欢迎使用MyukiGCard:", {
+				"author": "Stack Dev",
+				"github": "https://github.com/Uyukisan",
+				"usage": "https://github.com/Uyukisan/MyukiGCard",
+				"preview": "https://uyukisan.github.io/MyukiGCard/"
+			});
+			return {
+				"author": "Stack Dev",
+				"github": "https://github.com/Uyukisan",
+				"usage": "https://github.com/Uyukisan/MyukiGCard",
+				"preview": "https://uyukisan.github.io/MyukiGCard/"
+			};
 		}
 	}
-
 
 	function extend() {
 		var length = arguments.length;
@@ -205,7 +272,7 @@
 			target = {};
 		}
 		if (length == 1) {
-			target = this;
+			target = _this;
 			i--;
 		}
 		for (var i = 1; i < length; i++) {
@@ -249,7 +316,7 @@
 
 	}
 
-	
+
 	MyukiGCard.fn.init.prototype = MyukiGCard.fn;
 	window.MyukiGCard = MyukiGCard;
 	window.$MGC = MyukiGCard;
